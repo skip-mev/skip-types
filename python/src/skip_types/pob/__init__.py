4d14c278f0a750d3e89d59714a51fbb9f434e0f2
janfabian <mail@janfabian.cz>
2023-09-25 10:18:12 +0200

    feat(py): export MsgAuctionBid

---
+++ python/src/skip_types/pob/__init__.py
@@ -0,0 +1,3 @@
+from skip_types.pob.builder.v1.tx_pb2 import _globals
+
+MsgAuctionBid = _globals['MsgAuctionBid']
---
+++ python/src/skip_types/sdk/__init__.py
@@ -0,0 +1,3 @@
+from skip_types.sdk.auction.v1.tx_pb2 import _globals
+
+MsgAuctionBid = _globals['MsgAuctionBid']
