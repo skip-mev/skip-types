5d3f8d92e3ee7794a252b932b528ba6b1d8c383d
janfabian <mail@janfabian.cz>
2023-10-20 05:11:07 +0200

    feat(ts-example): bundle height

--- examples/typescript/index.mjs
+++ examples/typescript/index.mjs
@@ -6,7 +6,7 @@ import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx.js'
 import { MsgSend } from 'cosmjs-types/cosmos/bank/v1beta1/tx.js'
 
 const MNEMONIC = "...";
-const RPC_ENDPOINT = "https://juno-rpc.reece.sh";
+const RPC_ENDPOINT = "https://juno-rpc.polkachu.com";
 
 async function main() {
     const signer = await DirectSecp256k1HdWallet.fromMnemonic(MNEMONIC, {
@@ -23,87 +23,105 @@ async function main() {
     const currentBlockHeight = (await client.getBlock()).header.height;
 
     const { accountNumber, sequence } = await client.getSequence(myAddress);
-    
-    const transferMyselfNative = {
-      typeUrl: '/cosmos.bank.v1beta1.MsgSend',
-      value: MsgSend.fromPartial({
-        amount: [
-          {
-            amount: '1',
-            denom: 'ujuno',
-          },
-        ],
-        fromAddress: myAddress,
-        toAddress: myAddress,
-      }),
-    };
-    
-    const msgs = [transferMyselfNative];
-    
-    const txRaw = await client.signWithTimeout(
-      myAddress,
-      msgs,
-      {
-        amount: [
-          {
-            amount: '75000',
-            denom: 'ujuno',
-          },
-        ],
-        gas: '1000000',
-      },
-      '',
-      currentBlockHeight + 2,
-      {
-        accountNumber: accountNumber,
-        sequence: sequence + 1,
-        chainId: "juno-1",
-      },
-    );
-    
-    const auction = {
-      typeUrl: '/pob.builder.v1.MsgAuctionBid',
-    
-      value: MsgAuctionBid.fromPartial({
-        bid: {
-          amount: '1000000',
-          denom: 'ujuno',
+
+    async function bundle(height) {    
+      const transferMyselfNative = {
+        typeUrl: '/cosmos.bank.v1beta1.MsgSend',
+        value: MsgSend.fromPartial({
+          amount: [
+            {
+              amount: '1',
+              denom: 'ujuno',
+            },
+          ],
+          fromAddress: myAddress,
+          toAddress: myAddress,
+        }),
+      };
+      
+      const msgs = [transferMyselfNative];
+      
+      const txRaw = await client.signWithTimeout(
+        myAddress,
+        msgs,
+        {
+          amount: [
+            {
+              amount: '75000',
+              denom: 'ujuno',
+            },
+          ],
+          gas: '1000000',
         },
-        bidder: myAddress,
-        transactions: [TxRaw.encode(txRaw).finish()],
-      }),
-    };
-    
-    const txAuctionRaw = await client.signWithTimeout(
-      myAddress,
-      [auction],
-      {
-        amount: [
-          {
-            amount: '75000',
+        '',
+        height,
+        {
+          accountNumber: accountNumber,
+          sequence: sequence + 1,
+          chainId: "juno-1",
+        },
+      );
+      
+      const auction = {
+        typeUrl: '/pob.builder.v1.MsgAuctionBid',
+      
+        value: MsgAuctionBid.fromPartial({
+          bid: {
+            amount: '1000000',
             denom: 'ujuno',
           },
-        ],
-        gas: '1000000',
-      },
-      '',
-      currentBlockHeight + 2,
-      {
-        accountNumber: accountNumber,
-        sequence: sequence,
-        chainId: "juno-1",
-      },
-    );
-    
-    const r = await client.forceGetTmClient().broadcastTxSync({
-      tx: TxRaw.encode(txAuctionRaw).finish(),
-    });
-    
-    if (r.code !== 0) {
-      console.error(r);
-    } else {
-      console.log(Buffer.from(r.hash).toString("hex").toUpperCase());
+          bidder: myAddress,
+          transactions: [TxRaw.encode(txRaw).finish()],
+        }),
+      };
+      
+      const txAuctionRaw = await client.signWithTimeout(
+        myAddress,
+        [auction],
+        {
+          amount: [
+            {
+              amount: '75000',
+              denom: 'ujuno',
+            },
+          ],
+          gas: '1000000',
+        },
+        '',
+        height,
+        {
+          accountNumber: accountNumber,
+          sequence: sequence,
+          chainId: "juno-1",
+        },
+      );
+
+      return txAuctionRaw
     }
+
+    const [r1, r2] = await Promise.allSettled([
+      client.forceGetTmClient().broadcastTxSync({
+        tx: TxRaw.encode(await bundle(currentBlockHeight + 1)).finish(),
+      }), 
+      client.forceGetTmClient().broadcastTxSync({
+        tx: TxRaw.encode(await bundle(currentBlockHeight + 2)).finish(),
+      })
+    ])
+
+    let r;
+
+    if (r1.value?.code === 0) {
+      r = r1.value
+    } else if (r2.value?.code === 0) {
+      r = r2.value
+    }
+
+    if (r) {
+      console.log(Buffer.from(r.hash).toString("hex").toUpperCase());
+    } else {
+      console.error(r1, r2);
+    }
+
 }
 
 
